b95a300c7938c04093a81143f8554673a74e4f08
mskalisz <mskalisz3@gmail.com>
2022-07-09 11:39:27 -0400

    Update find_list query to the same format as updated all_lists query

--- database_persistence.rb
+++ database_persistence.rb
@@ -16,13 +16,26 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
+    sql = <<~SQL
+    SELECT lists.*, 
+      COUNT(todos.id) AS todos_count,
+      COUNT(NULLIF(todos.completed, false)) AS todos_remaining_count
+      FROM lists
+      LEFT JOIN todos ON todos.list_id = lists.id
+      WHERE lists.id = $1
+      GROUP BY lists.id
+      ORDER BY lists.name;
+  SQL
     result = query(sql, id)
 
     tuple = result.first
     list_id = tuple["id"].to_i
     todos = find_todos(list_id)
-    {id: list_id, name: tuple["name"], todos: todos}
+    { id: tuple["id"].to_i,
+      name: tuple["name"], 
+      todos: todos,
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i }
   end
 
   def all_lists
